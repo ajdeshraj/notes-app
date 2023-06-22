098c3ab34ed09c6b2252d751a33cf3add848b7b9
Arjun Deshraj <arjun.deshraj@gmail.com>
2023-06-22 15:21:18 +0530

    Adding yargs package and functionality

--- app.js
+++ app.js
@@ -1,11 +1,61 @@
+// Core modules
+// npm modules
 chalk = require('chalk')
+yargs = require('yargs')
+// Files
 getNotes = require('./notes.js')
 
-const cmd = process.argv[2]
+// Customizing yargs version
+yargs.version('1.1.0')
 
-if (cmd == 'add') {
-    console.log('Adding Note!')
-}
-else if (cmd == 'remove') {
-    console.log('Removing Note!')
-}
+// Creating commands
+// add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Title of Note',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Body of Note',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log('Title: '+argv.title)
+        console.log('Body: '+argv.body)
+    }
+})
+
+// remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Note Removed!')
+    }
+})
+
+// read command
+yargs.command({
+    command: 'read',
+    describe: 'Reading an existing note',
+    handler: function() {
+        console.log('Reading existing note!')
+    }
+})
+
+// list command
+yargs.command({
+    command: 'list',
+    describe: 'List titles of all existing notes',
+    handler: function() {
+        console.log('Listing titles of all notes')
+    }
+})
+
+yargs.parse()
